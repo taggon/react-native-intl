f1ef57371c1bd55fb397c98897b72b36e76ea087
Taegon Kim <gonom9@gmail.com>
2017-02-02 01:03:32 +0900

    Update the sample project for React Native 0.4.0

--- Examples/Hello/index.android.js
+++ Examples/Hello/index.android.js
@@ -1,26 +1,34 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * @flow
  */
-'use strict';
 
-const React = require('react-native');
-const { AppRegistry, StyleSheet, Text, View, Alert } = React;
-const Intl = require('react-native-intl');
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Alert
+} from 'react-native';
+import Intl from 'react-native-intl';
 
-var Hello = React.createClass({
-  getInitialState() {
-    return {
+export default class Hello extends Component {
+  constructor() {
+    super();
+    this.state = {
       _(msgid){ return msgid },
       today: '',
       number: '',
-      currency: ''
-    }
-  },
+      currency: '',
+    };
+    this.showLanguages = this.showLanguages.bind(this);
+  }
 
   componentDidMount() {
     this.onChangeLanguage('en-US');
-  },
+  }
 
   async onChangeLanguage(localeIdentifier) {
     const dateFormatter = new Intl.DateTimeFormat(localeIdentifier, {year:'numeric', month:'2-digit', day:'2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit', hour12:false});
@@ -32,47 +40,47 @@ var Hello = React.createClass({
       currency: await (new Intl.NumberFormat(localeIdentifier, {style:'currency'})).format(123456.78),
       _: await (new Intl.Translation(localeIdentifier)).getTranslator()
     });
-  },
+  }
 
   showLanguages() {
     Alert.alert(
       'Languages',
       '',
       [
-	{text:'English', onPress: ()=>this.onChangeLanguage('en-US') },
-	{text:'Français', onPress: ()=>this.onChangeLanguage('fr-FR') },
-	{text:'한국어', onPress: ()=>this.onChangeLanguage('ko-KR') }
+        {text:'English', onPress: ()=>this.onChangeLanguage('en-US') },
+        {text:'Français', onPress: ()=>this.onChangeLanguage('fr-FR') },
+        {text:'한국어', onPress: ()=>this.onChangeLanguage('ko-KR') }
       ]
     );
-  },
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.hello}>
-	  {this.state._("Hello, it's me")}
+          {this.state._("Hello, it's me")}
         </Text>
         <Text style={styles.date}>
-	  {this.state.today}
+          {this.state.today}
         </Text>
         <Text style={styles.number}>
-	  {this.state.integer}
+          {this.state.integer}
         </Text>
         <Text style={styles.number}>
-	  {this.state.number}
+          {this.state.number}
         </Text>
         <Text style={styles.number}>
-	  {this.state.currency}
+          {this.state.currency}
         </Text>
-	<Text onPress={this.showLanguages} style={styles.button}>
-	  {this.state._("Click to change your language")}
-	</Text>
+        <Text onPress={this.showLanguages} style={styles.button}>
+          {this.state._("Click to change your language")}
+        </Text>
       </View>
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -84,11 +92,22 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  date: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
   number: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  button: {
+    textAlign: 'center',
+    backgroundColor: '#dddddd',
+    padding: 15,
+    marginTop: 20
+  }
 });
 
 AppRegistry.registerComponent('Hello', () => Hello);
--- Examples/Hello/index.ios.js
+++ Examples/Hello/index.ios.js
@@ -1,37 +1,43 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * @flow
  */
-'use strict';
 
-const React = require('react-native');
-const { AppRegistry, StyleSheet, Text, View, ActionSheetIOS } = React;
-const Intl = require('react-native-intl');
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ActionSheetIOS
+} from 'react-native';
+import Intl from 'react-native-intl';
 
-var Hello = React.createClass({
-  getInitialState() {
-    return {
+export default class Hello extends Component {
+  constructor() {
+    super();
+    this.state = {
       _(msgid){ return msgid },
       today: '',
       number: '',
-      currency: ''
-    }
-  },
+      currency: '',
+    };
+    this.showActionSheet = this.showActionSheet.bind(this);
+  }
 
   componentDidMount() {
     this.onChangeLanguage('en-US');
-  },
+  }
 
   showActionSheet() {
     ActionSheetIOS.showActionSheetWithOptions(
-      {
-	options: ['English', 'Français', '한국어']
-      },
+      { options: [ 'English', 'Français', '한국어' ] },
       (buttonIndex) => {
-	this.onChangeLanguage(['en-US', 'fr-FR', 'ko-KR'][buttonIndex]);
+        this.onChangeLanguage(['en-US', 'fr-FR', 'ko-KR'][buttonIndex]);
       }
     );
-  },
+  }
 
   async onChangeLanguage(localeIdentifier) {
     const dateFormatter = new Intl.DateTimeFormat(localeIdentifier, {year:'numeric', month:'2-digit', day:'2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit', hour12:false});
@@ -43,35 +49,35 @@ var Hello = React.createClass({
       currency: await (new Intl.NumberFormat(localeIdentifier, {style:'currency'})).format(123456.78),
       _: await (new Intl.Translation(localeIdentifier)).getTranslator()
     });
-  },
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.hello}>
-	  {this.state._("Hello, it's me")}
+          {this.state._("Hello, it's me")}
         </Text>
         <Text style={styles.date}>
-	  {this.state.today}
+          {this.state.today}
         </Text>
         <Text style={styles.number}>
-	  {this.state.integer}
+          {this.state.integer}
         </Text>
         <Text style={styles.number}>
-	  {this.state.number}
+          {this.state.number}
         </Text>
         <Text style={styles.number}>
-	  {this.state.currency}
+          {this.state.currency}
         </Text>
-	<Text onPress={this.showActionSheet} style={styles.button}>
-	  {this.state._("Click to change your language")}
-	</Text>
+        <Text onPress={this.showActionSheet} style={styles.button}>
+          {this.state._("Click to change your language")}
+        </Text>
       </View>
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -83,11 +89,22 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  date: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
   number: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  button: {
+    textAlign: 'center',
+    backgroundColor: '#dddddd',
+    padding: 15,
+    marginTop: 20
+  }
 });
 
 AppRegistry.registerComponent('Hello', () => Hello);
